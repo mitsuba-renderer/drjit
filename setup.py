7903da68a7189edf19912a57a658f14a707e2db3
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-11-20 01:40:24 +0100

    clarified setup.py error message

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ import sys, re, os
 
 try:
     from skbuild import setup
+    import pybind11
 except ImportError:
-    print("Please update pip: version 10 or greater is required.",
+    print("The preferred way to invoke 'setup.py' is via pip, as in 'pip "
+          "install .'. If you wish to run the setup script directly, you must "
+          "first install the build dependencies listed in pyproject.toml!",
           file=sys.stderr)
     raise
 
