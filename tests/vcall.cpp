73d34b4b9eeda641e56cfce95ab2225b1db7f1cd
Sebastien Speierer <sebastieneps@gmail.com>
2021-03-03 10:34:48 +0100

    Clean tests/vcall.cpp

--- tests/vcall.cpp
+++ tests/vcall.cpp
@@ -7,23 +7,24 @@
 namespace ek = enoki;
 
 template <typename T> struct Struct {
-    T a;
-    T b;
+    T a, b;
     Struct(const T &a, const T &b) : a(a), b(b) { }
     ENOKI_STRUCT(Struct, a, b)
 };
 
-using Float = ek::CUDAArray<float>;
-using FloatD = ek::DiffArray<Float>;
-using UInt32 = ek::CUDAArray<uint32_t>;
-using UInt32D = ek::DiffArray<UInt32>;
-using Mask = ek::mask_t<Float>;
-using MaskD = ek::mask_t<FloatD>;
-using Array2f = ek::Array<Float, 2>;
+// using Float = ek::CUDAArray<float>;
+using Float = ek::LLVMArray<float>;
+
+using FloatD   = ek::DiffArray<Float>;
+using UInt32   = ek::uint32_array_t<Float>;
+using UInt32D  = ek::DiffArray<UInt32>;
+using Mask     = ek::mask_t<Float>;
+using MaskD    = ek::mask_t<FloatD>;
+using Array2f  = ek::Array<Float, 2>;
 using Array2fD = ek::Array<FloatD, 2>;
-using Array3f = ek::Array<Float, 3>;
+using Array3f  = ek::Array<Float, 3>;
 using Array3fD = ek::Array<FloatD, 3>;
-using StructF = Struct<Array3f>;
+using StructF  = Struct<Array3f>;
 using StructFD = Struct<Array3fD>;
 
 struct Base {
@@ -46,7 +47,7 @@ protected:
     Float x;
 };
 
-using BasePtr = ek::CUDAArray<Base *>;
+using BasePtr = ek::replace_scalar_t<Float, Base *>;
 
 struct A : Base {
     A(bool scalar) : Base(scalar) { ek::set_attr(this, "field", 2.4f); }
@@ -79,7 +80,10 @@ ENOKI_TEST(test01_vcall_reduce_and_record) {
     int n = 9999;
 
     // jit_set_log_level_stderr(::LogLevel::Error);
-    jit_init((uint32_t) JitBackend::CUDA);
+    if constexpr (ek::is_cuda_array_v<Float>)
+        jit_init((uint32_t) JitBackend::CUDA);
+    else
+        jit_init((uint32_t) JitBackend::LLVM);
 
     for (int i = 0; i < 3; ++i) {
         jit_set_flag(JitFlag::VCallRecord, i != 0);
@@ -129,50 +133,51 @@ ENOKI_TEST(test01_vcall_reduce_and_record) {
 ENOKI_TEST(test02_vcall_reduce_and_record_masked) {
     int n = 9999;
 
+    if constexpr (ek::is_cuda_array_v<Float>)
+        jit_init((uint32_t) JitBackend::CUDA);
+    else
+        jit_init((uint32_t) JitBackend::LLVM);
+
     ::Mask mask = ek::arange<UInt32>(n) > (n / 2);
 
     jit_set_log_level_stderr(::LogLevel::Error);
 
-    for (int k = 0; k < 2; ++k) {
-        jit_init((uint32_t) (k == 0 ? JitBackend::CUDA : JitBackend::LLVM));
+    for (int i = 0; i < 3; ++i) {
+        jit_set_flag(JitFlag::VCallRecord, i != 0);
+        jit_set_flag(JitFlag::VCallOptimize, i == 2);
 
-        for (int i = 0; i < 3; ++i) {
-            jit_set_flag(JitFlag::VCallRecord, i != 0);
-            jit_set_flag(JitFlag::VCallOptimize, i == 2);
+        for (int j = 0; j < 2; ++j) {
+            A *a = new A(j != 0);
+            B *b = new B(j != 0);
 
-            for (int j = 0; j < 2; ++j) {
-                A *a = new A(j != 0);
-                B *b = new B(j != 0);
+            ::Mask m = ek::neq(ek::arange<UInt32>(n) & 1, 0);
+            BasePtr arr = ek::select(m, (Base *) b, (Base *) a);
 
-                ::Mask m = ek::neq(ek::arange<UInt32>(n) & 1, 0);
-                BasePtr arr = ek::select(m, (Base *) b, (Base *) a);
+            StructF result = arr->f(Struct(Array3f(1, 2, 3) * ek::full<Float>(1, n),
+                                            Array3f(4, 5, 6) * ek::full<Float>(1, n)), mask);
 
-                StructF result = arr->f(Struct(Array3f(1, 2, 3) * ek::full<Float>(1, n),
-                                               Array3f(4, 5, 6) * ek::full<Float>(1, n)), mask);
+            assert(ek::all_nested(
+                ek::eq(result.a,
+                        ek::select(mask,
+                                    ek::select(m, Array3f(80.f, 100.f, 120.f),
+                                                Array3f(10.f, 20.f, 30.f)),
+                                    0.f)) &&
+                ek::eq(result.b,
+                        ek::select(mask,
+                                    ek::select(m, Array3f(10.f, 20.f, 30.f),
+                                                Array3f(60.f, 75.f, 90.f)),
+                                    0.f))));
 
-                assert(ek::all_nested(
-                    ek::eq(result.a,
-                           ek::select(mask,
-                                      ek::select(m, Array3f(80.f, 100.f, 120.f),
-                                                    Array3f(10.f, 20.f, 30.f)),
-                                      0.f)) &&
-                    ek::eq(result.b,
-                           ek::select(mask,
-                                      ek::select(m, Array3f(10.f, 20.f, 30.f),
-                                                    Array3f(60.f, 75.f, 90.f)),
-                                      0.f))));
+            // Masked pointer array
+            arr[!mask] = nullptr;
 
-                // Masked pointer array
-                arr[!mask] = nullptr;
+            StructF result2 = arr->f(Struct(Array3f(1, 2, 3) * ek::full<Float>(1, n),
+                                            Array3f(4, 5, 6) * ek::full<Float>(1, n)));
 
-                StructF result2 = arr->f(Struct(Array3f(1, 2, 3) * ek::full<Float>(1, n),
-                                                Array3f(4, 5, 6) * ek::full<Float>(1, n)));
+            assert(ek::all_nested(ek::eq(result.a, result2.a) && ek::eq(result.b, result2.b)));
 
-                assert(ek::all_nested(ek::eq(result.a, result2.a) && ek::eq(result.b, result2.b)));
-
-                delete a;
-                delete b;
-            }
+            delete a;
+            delete b;
         }
     }
 }
@@ -190,7 +195,7 @@ struct BaseD {
     FloatD x;
 };
 
-using BasePtrD = ek::DiffArray<ek::CUDAArray<BaseD *>>;
+using BasePtrD = ek::replace_scalar_t<FloatD, BaseD *>;
 
 struct AD : BaseD {
     StructFD f(const StructFD &m) override {
@@ -218,7 +223,10 @@ ENOKI_VCALL_METHOD(dummy)
 ENOKI_VCALL_END(BaseD)
 
 ENOKI_TEST(test03_vcall_symbolic_ad_fwd) {
-    jit_init((uint32_t) JitBackend::CUDA);
+    if constexpr (ek::is_cuda_array_v<Float>)
+        jit_init((uint32_t) JitBackend::CUDA);
+    else
+        jit_init((uint32_t) JitBackend::LLVM);
 
     AD *a = new AD();
     BD *b = new BD();
@@ -264,7 +272,11 @@ ENOKI_TEST(test03_vcall_symbolic_ad_fwd) {
 }
 
 ENOKI_TEST(test04_vcall_symbolic_ad_fwd_accessing_local) {
-    jit_init((uint32_t) JitBackend::CUDA);
+    if constexpr (ek::is_cuda_array_v<Float>)
+        jit_init((uint32_t) JitBackend::CUDA);
+    else
+        jit_init((uint32_t) JitBackend::LLVM);
+
     // jit_set_log_level_stderr(::LogLevel::Trace);
 
     for (int j = 0; j < 2; ++j) {
@@ -322,7 +334,11 @@ ENOKI_TEST(test04_vcall_symbolic_ad_fwd_accessing_local) {
 }
 
 ENOKI_TEST(test05_vcall_symbolic_ad_rev_accessing_local) {
-    jit_init((uint32_t) JitBackend::CUDA);
+    if constexpr (ek::is_cuda_array_v<Float>)
+        jit_init((uint32_t) JitBackend::CUDA);
+    else
+        jit_init((uint32_t) JitBackend::LLVM);
+
     // jit_set_log_level_stderr(::LogLevel::Trace);
 
     for (int i = 0; i < 3; ++i) {
