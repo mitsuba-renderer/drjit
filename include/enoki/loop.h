da280f8471d1c0c7f7919b0977ef6bdb2d6191f5
Wenzel Jakob <wenzel.jakob@epfl.ch>
2021-06-01 23:58:33 +0200

    updated to new ek.Loop implementation

--- include/enoki/loop.h
+++ include/enoki/loop.h
@@ -9,6 +9,7 @@
     All rights reserved. Use of this source code is governed by a BSD-style
     license that can be found in the LICENSE file.
 */
+
 #pragma once
 
 #include <enoki/array.h>
@@ -19,7 +20,7 @@ NAMESPACE_BEGIN(enoki)
 
 template <typename Mask, typename SFINAE = int> struct Loop;
 
-/// Scalar fallback
+/// Scalar fallback, expands into normal C++ loop
 template <typename Mask>
 struct Loop<Mask, enable_if_t<std::is_scalar_v<Mask>>> {
     Loop(const Loop &) = delete;
@@ -33,6 +34,7 @@ struct Loop<Mask, enable_if_t<std::is_scalar_v<Mask>>> {
     template <typename... Args> Loop(const char*, Args&...) { }
 };
 
+/// Array case, expands into a symbolic or wavefront-style loop
 template <typename Mask>
 struct Loop<Mask, enable_if_jit_array_t<Mask>> {
     static constexpr JitBackend Backend = Mask::Backend;
@@ -44,7 +46,7 @@ struct Loop<Mask, enable_if_jit_array_t<Mask>> {
 
     template <typename... Args>
     Loop(const char *name, Args &... args)
-        : m_state(0), m_cse_domain(jit_cse_domain(Backend)),
+        : m_state(0), m_cse_scope(jit_cse_scope(Backend)),
           m_se_offset((uint32_t) -1), m_se_flag(0), m_size(0),
           m_record(jit_flag(JitFlag::LoopRecord)) {
 
@@ -52,6 +54,7 @@ struct Loop<Mask, enable_if_jit_array_t<Mask>> {
         m_name = ek_unique_ptr<char[]>(new char[size]);
         memcpy(m_name.get(), name, size);
 
+        /// Immediately initialize if loop state is specified
         if constexpr (sizeof...(Args) > 0) {
             put(args...);
             init();
@@ -59,11 +62,19 @@ struct Loop<Mask, enable_if_jit_array_t<Mask>> {
     }
 
     ~Loop() {
+        if (m_state != 0 && m_state != 3 && m_state != 4)
+            jit_log(
+                LogLevel::Warn,
+                "enoki::Loop(\"%s\"): destructed in an inconsistent state. An "
+                "exception or disallowed scalar control flow (break, continue) "
+                "likely caused the loop to exit prematurely. Cleaning up..",
+                m_name.get());
+
         // Recover if an error occurred while recording a loop symbolically
         if (m_record && m_se_offset != (uint32_t) -1) {
             jit_side_effects_rollback(Backend, m_se_offset);
             jit_set_flag(JitFlag::Recording, m_se_flag);
-            jit_set_cse_domain(Backend, m_cse_domain);
+            jit_set_cse_scope(Backend, m_cse_scope);
 
             for (size_t i = 0; i < m_index_body.size(); ++i)
                 jit_var_dec_ref_ext(m_index_body[i]);
@@ -75,9 +86,8 @@ struct Loop<Mask, enable_if_jit_array_t<Mask>> {
                 jit_var_dec_ref_ext(m_index_out[i]);
         }
 
-        if (m_state != 0 && m_state != 3 && m_state != 4)
-            jit_log(LogLevel::Warn, "enoki::Loop(\"%s\"): destructed in an inconsistent state.", m_name.get());
-
+        jit_var_dec_ref_ext(m_loop_cond);
+        jit_var_dec_ref_ext(m_loop_body);
     }
 
     /// Register JIT variable indices of loop variables
@@ -102,7 +112,7 @@ struct Loop<Mask, enable_if_jit_array_t<Mask>> {
                     m_index_in.push_back(value.index());
                     m_invariant.push_back(0);
                     size_t size = value.size();
-                    if (m_size > 1 && size != 1 && size != m_size)
+                    if (m_size != 0 && size != 1 && size != m_size)
                         jit_raise("enoki::Loop::put(): loop variables have "
                                   "inconsistent sizes!");
                     if (size > m_size)
@@ -130,9 +140,42 @@ struct Loop<Mask, enable_if_jit_array_t<Mask>> {
                their state just before the loop condition is evaluated */
             m_se_flag = jit_flag(JitFlag::Recording);
             jit_set_flag(JitFlag::Recording, 1);
-            jit_new_cse_domain(Backend);
             m_se_offset = jit_side_effects_scheduled(Backend);
-            step();
+
+            /// New CSE scope for the loop condition
+            jit_new_cse_scope(Backend);
+
+            /// Copy loop state before entering loop (CUDA)
+            if (Backend == JitBackend::CUDA) {
+                for (size_t i = 0; i < m_index_p.size(); ++i) {
+                    uint32_t &index = *m_index_p[i];
+                    uint32_t dep[2] = { index };
+                    uint32_t next =
+                        jit_var_new_placeholder_loop("mov.$t0 $r0, $r1", 1, dep);
+                    jit_var_dec_ref_ext(index);
+                    index = next;
+                }
+            }
+
+            // Create a special node indicating the loop start
+            m_loop_cond = jit_var_new_stmt(Backend, VarType::Void, "", 1, 0, nullptr);
+            m_loop_cond = jit_var_resize(m_loop_cond, m_size);
+            jit_var_dec_ref_ext(m_loop_cond);
+
+            // Create phi nodes (LLVM)
+            if (Backend == JitBackend::LLVM) {
+                for (size_t i = 0; i < m_index_p.size(); ++i) {
+                    uint32_t &index = *m_index_p[i];
+                    uint32_t dep[2] = { index, m_loop_cond };
+                    uint32_t next = jit_var_new_placeholder_loop(
+                        "$r0 = phi <$w x $t0> [ $r0_final, %l_$i2_tail ], [ "
+                        "$r1, %l_$i2_start ]",
+                        2, dep);
+                    jit_var_dec_ref_ext(index);
+                    index = next;
+                }
+            }
+
             m_state = 1;
             jit_log(::LogLevel::InfoSym,
                     "enoki::Loop(\"%s\"): --------- begin recording loop ---------", m_name.get());
@@ -150,11 +193,11 @@ protected:
     struct MaskStackHelper {
     public:
         void push(uint32_t index) {
-            jit_var_mask_push(Mask::Backend, index);
+            jit_var_mask_push(Backend, index);
             m_armed = true;
         }
         void pop() {
-            jit_var_mask_pop(Mask::Backend);
+            jit_var_mask_pop(Backend);
             m_armed = false;
         }
         ~MaskStackHelper() {
@@ -179,12 +222,28 @@ protected:
                    variables using placeholders once more. They will represent
                    their state at the start of the loop body. */
                 m_cond = detach(cond);
-                step();
-                for (uint32_t i = 0; i < n; ++i) {
-                    uint32_t index = *m_index_p[i];
-                    m_index_body.push_back(index);
-                    jit_var_inc_ref_ext(index);
+
+                /// New CSE scope for the loop body
+                jit_new_cse_scope(Backend);
+                m_loop_body = jit_var_new_stmt(Backend, VarType::Void, "", 1, 1,
+                                               m_cond.index_ptr());
+
+                // Create phi nodes
+                for (size_t i = 0; i < m_index_p.size(); ++i) {
+                    uint32_t &index = *m_index_p[i];
+                    uint32_t dep[2] = { index, m_loop_cond };
+                    uint32_t next = jit_var_new_placeholder_loop(
+                        Backend == JitBackend::LLVM
+                            ? "$r0 = phi <$w x $t0> [ $r1, %l_$i2_cond ]"
+                            : "mov.$t0 $r0, $r1",
+                        2, dep);
+
+                    jit_var_dec_ref_ext(index);
+                    jit_var_inc_ref_ext(next);
+                    m_index_body.push_back(next);
+                    index = next;
                 }
+
                 m_state++;
                 if constexpr (Backend == JitBackend::LLVM)
                     m_mask_stack.push(m_cond.index());
@@ -197,7 +256,7 @@ protected:
                 for (uint32_t i = 0; i < n; ++i)
                     m_index_out.push_back(*m_index_p[i]);
 
-                jit_var_loop(m_name.get(), m_cond.index(),
+                jit_var_loop(m_name.get(), m_loop_cond, m_loop_body,
                              (uint32_t) n, m_index_body.data(),
                              m_index_out.data(), m_se_offset,
                              m_index_out.data(), m_state == 2,
@@ -253,7 +312,7 @@ protected:
 
                     m_index_out.clear();
                     jit_set_flag(JitFlag::Recording, m_se_flag);
-                    jit_set_cse_domain(Backend, m_cse_domain);
+                    jit_set_cse_scope(Backend, m_cse_scope);
                     m_se_offset = (uint32_t) -1;
                     m_cond = Mask();
                     m_state++;
@@ -278,9 +337,11 @@ protected:
     }
 
     bool cond_wavefront(const Mask &cond_) {
-        Mask cond = cond_; // Hold an extra reference!
+        Mask cond = cond_;
 
         if (m_cond.index()) {
+            cond &= m_cond;
+
             // Need to mask loop variables for disabled lanes
             m_mask_stack.pop();
             for (uint32_t i = 0; i < m_index_p.size(); ++i) {
@@ -290,7 +351,6 @@ protected:
                 jit_var_dec_ref_ext(i2);
             }
             m_index_out.clear();
-            m_cond = Mask();
         }
 
         // Ensure all loop state is evaluated
@@ -320,6 +380,12 @@ protected:
     /// A descriptive name
     ek_unique_ptr<char[]> m_name;
 
+    /// Bariable representing the start of a symbolic loop
+    uint32_t m_loop_cond = 0;
+
+    /// Bariable representing the body of a symbolic loop
+    uint32_t m_loop_body = 0;
+
     /// Pointers to loop variable indices
     ek_vector<uint32_t *> m_index_p;
 
@@ -344,19 +410,19 @@ protected:
     /// Index of the symbolic loop state machine
     uint32_t m_state;
 
-    /// CSE domain index
-    uint32_t m_cse_domain;
+    /// CSE scope index
+    uint32_t m_cse_scope;
 
     /// Offset in the side effects queue before the beginning of the loop
     uint32_t m_se_offset;
 
-    /// State of the Recording flag
+    /// State of the 'recording' JIT flag
     int m_se_flag;
 
     /// Keeps track of the size of loop variables to catch issues
     size_t m_size;
 
-    /// Is the loop being recorded symbolically
+    /// Is the loop being recorded symbolically?
     bool m_record;
 };
 
