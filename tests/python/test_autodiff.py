9a1554b176434aceb9fb53497c451e53baabd091
Sebastien Speierer <sebastieneps@gmail.com>
2022-05-17 17:22:28 +0200

    Fix dr.grad, dr.set_grad, dr.accum_grad

--- tests/python/test_autodiff.py
+++ tests/python/test_autodiff.py
@@ -16,10 +16,11 @@ def m(request):
 def struct_class(m):
     class MyStruct:
         def __init__(self) -> None:
-            self.x = m.Float()
-            self.y = m.Float()
+            self.x = m.Float(1.0)
+            self.y = m.Float(2.0)
         DRJIT_STRUCT = { 'x': m.Float, 'y': m.Float }
     return MyStruct
+
 
 def test01_enable_grad(m):
     # Test on single float variable
@@ -77,7 +78,7 @@ def test02_detach(m):
     dr.enable_grad(a)
     b = dr.detach(a, preserve_type=False)
     c = dr.detach(a, preserve_type=True)
-    assert not type(a) == type(b)
+    assert dr.detached_t(type(a)) == type(b)
     assert type(a) == type(c)
     assert dr.grad_enabled(a)
     assert not dr.grad_enabled(b)
@@ -87,7 +88,7 @@ def test02_detach(m):
     dr.enable_grad(a)
     b = dr.detach(a, preserve_type=False)
     c = dr.detach(a, preserve_type=True)
-    assert not type(a) == type(b)
+    assert dr.detached_t(type(a)) == type(b)
     assert type(a) == type(c)
     assert dr.grad_enabled(a)
     assert not dr.grad_enabled(b)
@@ -100,21 +101,128 @@ def test02_detach(m):
     assert dr.grad_enabled(a)
     assert not dr.grad_enabled(c)
 
+    with pytest.raises(TypeError) as ei:
+        dr.detach(a, preserve_type=False)
+    assert "required to preserve the input type" in str(ei.value)
 
-def test03_set_accum_grad(m):
+
+def test03_set_grad(m):
     a = m.Float([1, 2, 3])
-
     dr.enable_grad(a)
     assert dr.allclose(dr.grad(a), 0.0)
-
     dr.set_grad(a, 2.0)
     assert dr.allclose(dr.grad(a), 2.0)
-
-    dr.accum_grad(a, 2.0)
-    assert dr.allclose(dr.grad(a), 4.0)
-
+    dr.set_grad(a, m.Float([3.0]))
+    assert dr.allclose(dr.grad(a), 3.0)
+    with pytest.raises(RuntimeError) as ei:
+        dr.set_grad(a, m.Float([1, 2, 3, 4]))
+    assert "attempted to assign a gradient of size 4" in str(ei.value)
     dr.set_grad(a, [3.0, 2.0, 1.0])
-    assert dr.allclose(dr.grad(a), m.Float([3.0, 2.0, 1.0]))
+    assert dr.allclose(dr.grad(a), [3.0, 2.0, 1.0])
+
+    a = m.Array3f([1, 2, 3], [2, 3, 4], [3, 4, 5])
+    dr.enable_grad(a)
+    assert dr.allclose(dr.grad(a), 0.0)
+    dr.set_grad(a, 2.0)
+    assert dr.allclose(dr.grad(a), 2.0)
+    dr.set_grad(a, m.Float([3.0]))
+    assert dr.allclose(dr.grad(a), 3.0)
+    with pytest.raises(RuntimeError) as ei:
+        dr.set_grad(a, m.Float([1, 2, 3, 4]))
+    assert "attempted to assign a gradient of size 4" in str(ei.value)
+    dr.set_grad(a, m.Array3f([3, 2, 1]))
+    assert dr.allclose(dr.grad(a.y), [2, 2, 2])
+    dr.set_grad(a, m.Array3f([1, 2, 3], [2, 3, 4], [3, 4, 5]))
+    assert dr.allclose(dr.grad(a), [[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+
+    args = [m.Float(1.0), m.Float(2.0), m.Float(3.0)]
+    dr.enable_grad(args)
+    dr.set_grad(args, 1.0)
+    assert dr.allclose(dr.grad(args), 1.0)
+    dr.set_grad(args, [3.0, 2.0, 1.0])
+    assert dr.allclose(dr.grad(args), [3.0, 2.0, 1.0])
+    with pytest.raises(RuntimeError) as ei:
+        dr.set_grad(args, [3.0, 2.0])
+    assert "argument sizes are not matching" in str(ei.value)
+
+    args = {'a': m.Float(1.0), 'b': m.Float(2.0)}
+    dr.enable_grad(args)
+    dr.set_grad(args, 1.0)
+    assert dr.allclose(dr.grad(args)['a'], 1.0)
+    assert dr.allclose(dr.grad(args)['b'], 1.0)
+    dr.set_grad(args, {'a': 2.0, 'b': 3.0})
+    assert dr.allclose(dr.grad(args)['a'], 2.0)
+    assert dr.allclose(dr.grad(args)['b'], 3.0)
+
+    a = struct_class(m)()
+    dr.enable_grad(a)
+    dr.set_grad(a, 1.0)
+    assert dr.allclose(dr.grad(a).x, 1.0)
+    assert dr.allclose(dr.grad(a).y, 1.0)
+    dr.set_grad(a, struct_class(m)())
+    assert dr.allclose(dr.grad(a).x, 1.0)
+    assert dr.allclose(dr.grad(a).y, 2.0)
+
+
+def test04_accum_grad(m):
+    a = m.Float([1, 2, 3])
+    dr.enable_grad(a)
+    assert dr.allclose(dr.grad(a), 0.0)
+    dr.accum_grad(a, 2.0)
+    assert dr.allclose(dr.grad(a), 2.0)
+    dr.accum_grad(a, m.Float([3.0]))
+    assert dr.allclose(dr.grad(a), 5.0)
+    with pytest.raises(RuntimeError) as ei:
+        dr.accum_grad(a, m.Float([1, 2, 3, 4]))
+    assert "attempted to accumulate a gradient of size 4" in str(ei.value)
+    dr.accum_grad(a, [3.0, 2.0, 1.0])
+    assert dr.allclose(dr.grad(a), [8.0, 7.0, 6.0])
+
+    a = m.Array3f([1, 2, 3], [2, 3, 4], [3, 4, 5])
+    dr.enable_grad(a)
+    assert dr.allclose(dr.grad(a), 0.0)
+    dr.accum_grad(a, 2.0)
+    assert dr.allclose(dr.grad(a), 2.0)
+    dr.accum_grad(a, m.Float([3.0]))
+    assert dr.allclose(dr.grad(a), 5.0)
+    with pytest.raises(RuntimeError) as ei:
+        dr.accum_grad(a, m.Float([1, 2, 3, 4]))
+    assert "attempted to accumulate a gradient of size 4" in str(ei.value)
+    dr.accum_grad(a, m.Array3f([3, 2, 1]))
+    assert dr.allclose(dr.grad(a.y), [7, 7, 7])
+    dr.accum_grad(a, m.Array3f([1, 2, 3], [2, 3, 4], [3, 4, 5]))
+    assert dr.allclose(dr.grad(a), [[9, 10, 11], [9, 10, 11], [9, 10, 11]])
+
+    args = [m.Float(1.0), m.Float(2.0), m.Float(3.0)]
+    dr.enable_grad(args)
+    dr.accum_grad(args, 1.0)
+    assert dr.allclose(dr.grad(args), 1.0)
+    dr.accum_grad(args, [3.0, 2.0, 1.0])
+    assert dr.allclose(dr.grad(args), [4.0, 3.0, 2.0])
+    with pytest.raises(RuntimeError) as ei:
+        dr.accum_grad(args, [3.0, 2.0])
+    assert "argument sizes are not matching" in str(ei.value)
+
+    args = {'a': m.Float(1.0), 'b': m.Float(2.0)}
+    dr.enable_grad(args)
+    dr.accum_grad(args, 1.0)
+    assert dr.allclose(dr.grad(args)['a'], 1.0)
+    assert dr.allclose(dr.grad(args)['b'], 1.0)
+    dr.accum_grad(args, {'a': 2.0, 'b': 3.0})
+    assert dr.allclose(dr.grad(args)['a'], 3.0)
+    assert dr.allclose(dr.grad(args)['b'], 4.0)
+
+    a = struct_class(m)()
+    dr.enable_grad(a)
+    dr.accum_grad(a, 1.0)
+    assert dr.allclose(dr.grad(a).x, 1.0)
+    assert dr.allclose(dr.grad(a).y, 1.0)
+    dr.accum_grad(a, struct_class(m)())
+    assert dr.allclose(dr.grad(a).x, 2.0)
+    assert dr.allclose(dr.grad(a).y, 3.0)
+
+
+# TODO ------------
 
 def test04_forward_from(m):
     a = m.Float(1.0)
@@ -122,6 +230,7 @@ def test04_forward_from(m):
     b = a * a * 2
     dr.forward(a)
     assert dr.allclose(dr.grad(b), 4.0)
+
 
 def test05_backward_from(m):
     a = m.Float(1.0)
@@ -156,23 +265,15 @@ def test47_replace_grad(m):
     assert dr.allclose(dr.grad(x), 0)
     assert dr.allclose(dr.grad(y), [12, 32, 36])
 
-
-if __name__ == "__main__":
-    from drjit.llvm.ad import Float
-
-    a = Float(4.0)
-
-    print(a)
-
-    print(dr.grad_enabled(a))
-
-    dr.enable_grad(a)
-
-    print(dr.grad_enabled(a))
-
-
-    dr.disable_grad(a)
-
-    print(dr.grad_enabled(a))
-
-    # assert True
+# TODO test dr.detach
+# TODO test dr.grad
+# TODO test dr.replace_grad
+# TODO test dr.forward_to
+# TODO test dr.forward_from
+# TODO test dr.forward
+# TODO test dr.backward
+# TODO test dr.backward_to
+# TODO test dr.backward_from
+# TODO test dr.set_label
+# TODO test dr.graphviz_ad
+# TODO test all ad derivatives (math arithmetic) -> see test_ad.py
