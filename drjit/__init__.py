75a58605950a59a7954d594486d864806323c1aa
Nicolas Roussel <n.roussel13@gmail.com>
2022-09-08 08:27:40 +0200

    Remove additional runtime DLL directories (only useful for local mitsuba builds)

--- drjit/__init__.py
+++ drjit/__init__.py
@@ -4,17 +4,6 @@ import os
 if sys.version_info < (3, 8):
     raise ImportError("Dr.Jit requires Python >= 3.8")
 
-if os.name == 'nt':
-    # Specify DLL search path for windows (no rpath on this platform..)
-    d = __file__
-    for i in range(3):
-        d = os.path.dirname(d)
-    try: # try to use Python 3.8's DLL handling
-        os.add_dll_directory(d)
-    except AttributeError:  # otherwise use PATH
-        os.environ['PATH'] += os.pathsep + d
-    del d, i
-
 # Implementation details accessed by both C++ and Python
 import drjit.detail as detail # noqa
 
