15c8e9c3e29b2234e15fc6ce9211e68682c8a869
Nicolas Roussel <n.roussel13@gmail.com>
2024-02-29 15:05:38 +0100

    Fixes to DLPack interfaces for dynamic arrays

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,15 +1,19 @@
 import drjit as dr
 import pytest
 
-# Test conversions to/from numpy (tensors)
-@pytest.test_arrays('tensor, -bool, -float16')
-def test01_roundtrip_tensor_numpy(t):
+# Test conversions to/from numpy (tensors & dynamic arrays)
+@pytest.test_arrays('is_tensor, -bool, -float16')
+def test01_roundtrip_dynamic_numpy(t):
     pytest.importorskip("numpy")
     a = t([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
     roundtrip = t(a.numpy())
 
     assert roundtrip.shape == (2, 2, 3) and roundtrip.shape == a.shape
     assert dr.all(a == roundtrip, axis=None)
+
+    flat_t = type(a.array)
+    roundtrip = flat_t(a.array.numpy())
+    assert dr.all(a.array == roundtrip, axis=None)
 
 # Test conversions to/from numpy (vectors)
 @pytest.test_arrays('vector, shape=(3, *), -bool, -float16')
@@ -21,15 +25,19 @@ def test02_roundtrip_vector_numpy(t):
     assert roundtrip.shape == (3, 3) and roundtrip.shape == a.shape
     assert dr.all(a == roundtrip, axis=None)
 
-# Test conversions to/from torch (tensors)
+# Test conversions to/from torch (tensors & dynamic array)
 @pytest.test_arrays('tensor, -bool, -float16, -uint64, -uint32')
-def test03_roundtrip_tensor_torch(t):
+def test03_roundtrip_dynamic_torch(t):
     pytest.importorskip("torch")
     a = t([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
     roundtrip = t(a.torch())
 
     assert roundtrip.shape == (2, 2, 3) and roundtrip.shape == a.shape
     assert dr.all(a == roundtrip, axis=None)
+
+    flat_t = type(a.array)
+    roundtrip = flat_t(a.array.numpy())
+    assert dr.all(a.array == roundtrip, axis=None)
 
 # Test conversions to/from torch (vectors)
 @pytest.test_arrays('vector, shape=(3, *), -bool, -uint64, -uint32')
@@ -41,42 +49,50 @@ def test04_roundtrip_vector_torch(t):
     assert roundtrip.shape == (3, 3) and roundtrip.shape == a.shape
     assert dr.all(a == roundtrip, axis=None)
 
-# Test conversions to/from tf (tensors)
+# Test conversions to/from tf (tensors & dynamic array)
 @pytest.test_arrays('tensor, -bool, -float16')
-def test05_roundtrip_tensor_tf(t):
+def test05_roundtrip_dynamic_tf(t):
     pytest.importorskip("tensorflow")
-    arr = t([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-    roundtrip = t(arr.tf())
+    a = t([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+    roundtrip = t(a.tf())
 
-    assert roundtrip.shape == (2, 2, 3) and roundtrip.shape == arr.shape
-    assert dr.all(arr == roundtrip, axis=None)
+    assert roundtrip.shape == (2, 2, 3) and roundtrip.shape == a.shape
+    assert dr.all(a == roundtrip, axis=None)
+
+    flat_t = type(a.array)
+    roundtrip = flat_t(a.array.numpy())
+    assert dr.all(a.array == roundtrip, axis=None)
 
 # Test conversions to/from tf (vectors)
 @pytest.test_arrays('vector, shape=(3, *), -bool, -float16')
 def test06_roundtrip_vector_tf(t):
     pytest.importorskip("tensorflow")
-    arr = t([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    roundtrip = t(arr.tf())
+    a = t([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    roundtrip = t(a.tf())
 
-    assert roundtrip.shape == (3, 3) and roundtrip.shape == arr.shape
-    assert dr.all(arr == roundtrip, axis=None)
+    assert roundtrip.shape == (3, 3) and roundtrip.shape == a.shape
+    assert dr.all(a == roundtrip, axis=None)
 
-# Test conversions to/from jax (tensors)
+# Test conversions to/from jax (tensors & dynamic array)
 @pytest.test_arrays('tensor, -bool, -uint64, -int64, -float64')
-def test07_roundtrip_tensor_jax(t):
+def test07_roundtrip_dynamic_jax(t):
     pytest.importorskip("jax")
-    arr = t([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-    roundtrip = t(arr.jax())
+    a = t([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+    roundtrip = t(a.jax())
 
-    assert roundtrip.shape == (2, 2, 3) and roundtrip.shape == arr.shape
-    assert dr.all(arr == roundtrip, axis=None)
+    assert roundtrip.shape == (2, 2, 3) and roundtrip.shape == a.shape
+    assert dr.all(a == roundtrip, axis=None)
+
+    flat_t = type(a.array)
+    roundtrip = flat_t(a.array.numpy())
+    assert dr.all(a.array == roundtrip, axis=None)
 
 # Test conversions to/from jax(vectors)
 @pytest.test_arrays('vector, shape=(3, *), -bool, -uint64, -int64, -float64')
 def test08_roundtrip_vector_jax(t):
     pytest.importorskip("jax")
-    arr = t([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    roundtrip = t(arr.jax())
+    a = t([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    roundtrip = t(a.jax())
 
-    assert roundtrip.shape == (3, 3) and roundtrip.shape == arr.shape
-    assert dr.all(arr == roundtrip, axis=None)
+    assert roundtrip.shape == (3, 3) and roundtrip.shape == a.shape
+    assert dr.all(a == roundtrip, axis=None)
