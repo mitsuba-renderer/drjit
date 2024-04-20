497fafe62d5f82112e8cdeb0df3d248a9fa7c784
Wenzel Jakob <wenzel.jakob@epfl.ch>
2024-04-20 12:13:42 +0200

    test gathers in nested symbolic/evaluated loops, fwd mode

--- tests/test_while_loop_ad.py
+++ tests/test_while_loop_ad.py
@@ -5,7 +5,8 @@ import pytest
 @pytest.mark.parametrize("optimize", [True, False])
 @pytest.test_arrays('float,is_diff,shape=(*)')
 @dr.syntax
-def test01_simple_diff_loop(t, optimize, mode):
+def test01_simple_diff_loop_fwd(t, optimize, mode):
+    # Forward-mode derivative of a simple loop with 1 differentiable state variable
     i, j = dr.int32_array_t(t)(0), t(1)
     dr.enable_grad(j)
     dr.set_grad(j, 1.1)
@@ -20,7 +21,8 @@ def test01_simple_diff_loop(t, optimize, mode):
 @pytest.mark.parametrize('mode', ['evaluated', 'symbolic'])
 @pytest.mark.parametrize("optimize", [True, False])
 @pytest.test_arrays('float,is_diff,shape=(*)')
-def test02_complex_diff_loop(t, optimize, mode):
+def test02_complex_diff_loop_fwd(t, optimize, mode):
+    # Forward-mode derivative of a loop with more complex variable dependences
     i = dr.int32_array_t(t)(0)
     lvars = [t(0) for i in range(10)]
     dr.enable_grad(lvars[5])
@@ -39,6 +41,9 @@ def test02_complex_diff_loop(t, optimize, mode):
 @pytest.test_arrays('float32,diff,shape=(*)')
 @dr.syntax
 def test03_sum_loop_fwd(t, mode):
+    # Tests a simple sum loop that adds up a differentiable quantity
+    # Forward-mode is the easy case, and the test just exists here
+    # as cross-check for the reverse-mode version below
     UInt32 = dr.uint32_array_t(t)
     Float = t
 
@@ -61,6 +66,8 @@ def test03_sum_loop_fwd(t, mode):
 @pytest.test_arrays('float32,diff,shape=(*)')
 @dr.syntax
 def test04_sum_loop_rev(t, mode):
+    # Test the "sum loop" optimization (max_iterations=-1) for
+    # consistency against test03
     UInt32 = dr.uint32_array_t(t)
     Float = t
 
@@ -78,3 +85,87 @@ def test04_sum_loop_rev(t, mode):
 
     assert dr.allclose(y, [1.33333, 1.99805, 3.77475, 10])
     assert dr.allclose(xo.grad, [1.77773, 3.95703, 12.0956, 45])
+
+@pytest.mark.parametrize('variant', ['fwd', 'bwd'])
+@pytest.test_arrays('float32,is_diff,shape=(*)')
+def test05_evaluated_ad_kernel_launch_count(t, variant):
+    # Check that the forward/reverse-mode derivative of an
+    # evaluated loop launches a similar number of kernels
+    UInt = dr.uint32_array_t(t)
+
+    x = t(2,3,4,5)
+    dr.enable_grad(x)
+    iterations = 50
+
+    with dr.scoped_set_flag(dr.JitFlag.KernelHistory):
+        dr.kernel_history_clear()
+        _, y, i = dr.while_loop(
+            state=(x, t(1, 1, 1, 1), dr.zeros(UInt, 4)),
+            cond=lambda x, y, i: i<iterations,
+            body=lambda x, y, i: (x, .5*(y + x/y), i + 1),
+            labels=('x', 'y', 'i'),
+            mode='evaluated'
+        )
+        h = dr.kernel_history((dr.KernelType.JIT,))
+
+    from math import sqrt
+    assert len(h) >= iterations and len(h) < iterations + 3
+    assert dr.allclose(y, (sqrt(2), sqrt(3), sqrt(4), sqrt(5)))
+
+    if variant == 'fwd':
+        x.grad = dr.opaque(t, 1)
+        with dr.scoped_set_flag(dr.JitFlag.KernelHistory):
+            g = dr.forward_to(y)
+            dr.eval(g)
+            h = dr.kernel_history((dr.KernelType.JIT,))
+    elif variant == 'bwd':
+        y.grad = dr.opaque(t, 1)
+        with dr.scoped_set_flag(dr.JitFlag.KernelHistory):
+            g = dr.backward_to(x)
+            dr.eval(g)
+            h = dr.kernel_history((dr.KernelType.JIT,))
+    else:
+        raise Exception('internal error')
+    assert dr.allclose(g, (1/(2*sqrt(2)), 1/(2*sqrt(3)), 1/(2*sqrt(4)), 1/(2*sqrt(5))))
+    assert len(h) >= iterations and len(h) < iterations + 3
+    for k in h:
+        assert k['operation_count'] < iterations
+
+@pytest.mark.parametrize('mode', ['evaluated', 'symbolic'])
+@pytest.test_arrays('float32,diff,shape=(*)')
+@dr.syntax
+def test06_gather_in_loop_fwd(t, mode):
+    x = dr.opaque(t, 0, 3)
+    xo = x
+    dr.enable_grad(x)
+    i = dr.uint32_array_t(x)(0)
+    y = dr.zeros(t)
+    while dr.hint(i < 3, mode=mode):
+        y += dr.gather(t, x, i)*i
+        i += 1
+    xo.grad=[1,2,3]
+    dr.forward_to(y)
+    assert y.grad == 8
+
+
+@pytest.mark.parametrize('mode', ['evaluated', 'symbolic'])
+@pytest.test_arrays('float32,diff,shape=(*)')
+@dr.syntax
+def test07_gather_in_loop_fwd_nested(t, mode):
+    UInt = dr.uint32_array_t(t)
+    x = dr.opaque(t, 0, 3)
+    x.label="x"
+    xo = x
+    dr.enable_grad(x)
+    y = dr.zeros(t)
+    j = dr.zeros(UInt, 3)
+    while dr.hint(j < 2, mode=mode):
+        i = dr.zeros(UInt, 3)
+        while dr.hint(i < 3, mode=mode):
+            y += dr.gather(t, x, i)*i
+            i += 1
+        j += 1
+    xo.grad=[1,2,3]
+    dr.forward_to(y)
+    assert dr.all(y.grad == 16)
+
