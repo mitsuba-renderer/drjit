a60b604c9ba68d91c36b009f3423f12b77cbcac8
Wenzel Jakob <wenzel.jakob@epfl.ch>
2024-06-04 12:08:42 +0200

    fix GraphViz output

--- src/extra/autodiff.cpp
+++ src/extra/autodiff.cpp
@@ -3357,9 +3357,9 @@ const char *ad_var_graphviz() {
     buffer.clear();
     buffer.put("digraph {\n"
                "    rankdir=BT;\n"
-               "    graph [dpi=50 fontname=Consolas];\n"
-               "    node [shape=record fontname=Consolas];\n"
-               "    edge [fontname=Consolas];\n");
+               "    graph [fontname=\"Ubuntu Mono\"];\n"
+               "    node [shape=record fontname=\"Ubuntu Mono\"];\n"
+               "    edge [fontname=\"Ubuntu Mono\"];\n");
 
     size_t current_hash = 0, current_depth = 1;
     std::hash<std::string> hasher;
