d668f5a1c66efd58af52dab01ba38b38b820ba7d
Wenzel Jakob <wenzel.jakob@epfl.ch>
2024-10-16 22:30:00 +0900

    ``dr.if_while_loop()``: maintain AD status of unchanged variables in AD-suspended mode
    Dr.Jit control flow operations (``dr.if_stmt(), drjit.while_loop()``)
    disable gradient tracking of all variable state when the operation takes
    place within an AD-disabled scope.
    
    This can be surprising when a ``@dr.syntax`` transformation silently
    passes local variables to such an operation, which then become
    non-differentiable.
    
    This commit carves out an exception: when variables aren't actually
    modified by the control flow operation, they can retain their AD
    identity.
    
    This is part #2 of the fix for issue #253 reported by @dvicini and
    targets ``dr.while_lop()`` only. The previous commit fixed the same
    problem for ``if`` statements.

--- src/extra/loop.cpp
+++ src/extra/loop.cpp
@@ -155,6 +155,7 @@ static size_t ad_loop_evaluated_mask(JitBackend backend, const char *name,
     index64_vector indices2;
     JitVar active_it;
     size_t it = 0;
+    bool grad_suspended = ad_grad_suspended();
 
     while (true) {
         // Evaluate the loop state
@@ -189,8 +190,15 @@ static size_t ad_loop_evaluated_mask(JitBackend backend, const char *name,
         }
 
         for (size_t i = 0; i < indices2.size(); ++i) {
-            uint64_t i1 = indices2[i];
-            uint64_t i2 = ad_var_copy(i1);
+            // Kernel caching: Must create an AD copy so that gradient
+            // computation steps involving this variable (even if unchangecd
+            // & only used as a read-only dependency) are correctly placed
+            // within their associated loop iterations. This does not create
+            // a copy of the underlying JIT variable.
+
+            uint64_t i1 = indices2[i],
+                     i2 = grad_suspended ? ad_var_inc_ref(i1) : ad_var_copy(i1);
+
             ad_var_dec_ref(i1);
             ad_mark_loop_boundary(i2);
             int unused = 0;
