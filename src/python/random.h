2299975196d1730125613bb78c36dcdaff1e2eae
Wenzel Jakob <wenzel.jakob@epfl.ch>
2025-02-19 23:54:23 +0900

    Normal random number generation
    This commit adds the functions ``PCG32.sample_float32_n`` and
    ``PCG32.sample_float64_n`` to generate random variates with a standard
    normal distribution.
    
    On top of this, the commit adds the following bindings
    
    - ``PCG32.sample_float()``
    - ``PCG32.sample_float_n()``
    
    They take a type (and optionally, a mask) and then automatically decide
    between the ``float32`` and ``float64`` variants.

--- src/python/random.h
+++ src/python/random.h
@@ -2,12 +2,14 @@
 
 #include <drjit/random.h>
 #include "common.h"
+#include "base.h"
 
 template <typename Guide>
 void bind_pcg32(nb::module_ &m) {
     using UInt64 = dr::uint64_array_t<Guide>;
     using Int64 = dr::int64_array_t<Guide>;
     using PCG32 = dr::PCG32<UInt64>;
+    using Mask = dr::mask_t<UInt64>;
 
     auto pcg32 = nb::class_<PCG32>(m, "PCG32", doc_PCG32)
         .def(nb::init<size_t, const UInt64 &, const UInt64 &>(),
@@ -18,22 +20,84 @@ void bind_pcg32(nb::module_ &m) {
         .def("seed", &PCG32::seed,
              "initstate"_a.sig("UInt64(0x853c49e6748fea9b)")  = PCG32_DEFAULT_STATE,
              "initseq"_a.sig("UInt64(0xda3e39cb94b95bdb)") = PCG32_DEFAULT_STREAM, doc_PCG32_seed)
-        .def("next_uint32", nb::overload_cast<>(&PCG32::next_uint32), doc_PCG32_next_uint32)
         .def("next_uint32",
-             nb::overload_cast<const dr::mask_t<UInt64> &>(&PCG32::next_uint32))
-        .def("next_uint32_bounded", &PCG32::next_uint32_bounded, "bound"_a,
-             "mask"_a.sig("Bool(True)") = true, doc_PCG32_next_uint32_bounded)
-        .def("next_uint64", nb::overload_cast<>(&PCG32::next_uint64), doc_PCG32_next_uint64)
+             nb::overload_cast<>(&PCG32::next_uint32),
+             doc_PCG32_next_uint32)
+        .def("next_uint32",
+             nb::overload_cast<const Mask &>(&PCG32::next_uint32))
+        .def("next_uint32_bounded",
+             &PCG32::next_uint32_bounded, "bound"_a, "mask"_a.sig("Bool(True)") = true,
+             doc_PCG32_next_uint32_bounded)
         .def("next_uint64",
-             nb::overload_cast<const dr::mask_t<UInt64> &>(&PCG32::next_uint64))
+             nb::overload_cast<>(&PCG32::next_uint64),
+             doc_PCG32_next_uint64)
+        .def("next_uint64",
+             nb::overload_cast<const Mask &>(&PCG32::next_uint64))
         .def("next_uint64_bounded", &PCG32::next_uint64_bounded, "bound"_a,
-             "mask"_a.sig("Bool(True)") = true, doc_PCG32_next_uint64_bounded)
-        .def("next_float32", nb::overload_cast<>(&PCG32::next_float32))
-        .def("next_float32", nb::overload_cast<const dr::mask_t<UInt64> &>(
-                                 &PCG32::next_float32), doc_PCG32_next_float32)
-        .def("next_float64", nb::overload_cast<>(&PCG32::next_float64))
-        .def("next_float64", nb::overload_cast<const dr::mask_t<UInt64> &>(
-                                 &PCG32::next_float64), doc_PCG32_next_float64)
+             "mask"_a.sig("Bool(True)") = true,
+             doc_PCG32_next_uint64_bounded)
+        .def("next_float",
+             [](nb::handle self, nb::type_object dtype, nb::handle mask) {
+                 const char *key = nullptr;
+                 if constexpr (dr::is_jit_v<Guide>) {
+                     if (is_drjit_type(dtype)) {
+                         const ArraySupplement &s = supp(dtype);
+                         if ((VarType) s.type == VarType::Float32)
+                             key = "next_float32";
+                         else if ((VarType) s.type == VarType::Float64)
+                             key = "next_float64";
+                     }
+                 } else if (dtype.is(&PyFloat_Type)) {
+                     key = "next_float64";
+                 }
+
+                 if (!key)
+                     nb::raise_type_error("Invalid 'dtype'");
+
+                 auto &&fn = self.attr(key);
+                 return !mask.is(Py_True) ? fn(mask) : fn();
+             }, "dtype"_a, "mask"_a = true, doc_PCG32_next_float)
+        .def("next_float32",
+             nb::overload_cast<>(&PCG32::next_float32),
+             doc_PCG32_next_float32)
+        .def("next_float32",
+             nb::overload_cast<const Mask &>(&PCG32::next_float32))
+        .def("next_float64",
+             nb::overload_cast<>(&PCG32::next_float64),
+             doc_PCG32_next_float64)
+        .def("next_float64",
+             nb::overload_cast<const Mask &>(&PCG32::next_float64))
+        .def("next_float_n",
+             [](nb::handle self, nb::type_object dtype, nb::handle mask) {
+                 const char *key = nullptr;
+                 if constexpr (dr::is_jit_v<Guide>) {
+                     if (is_drjit_type(dtype)) {
+                         const ArraySupplement &s = supp(dtype);
+                         if ((VarType) s.type == VarType::Float32)
+                             key = "next_float32_n";
+                         else if ((VarType) s.type == VarType::Float64)
+                             key = "next_float64_n";
+                     }
+                 } else if (dtype.is(&PyFloat_Type)) {
+                     key = "next_float64_n";
+                 }
+
+                 if (!key)
+                     nb::raise_type_error("Invalid 'dtype'");
+
+                 auto &&fn = self.attr(key);
+                 return !mask.is(Py_True) ? fn(mask) : fn();
+             }, "dtype"_a, "mask"_a = true, doc_PCG32_next_float_n)
+        .def("next_float32_n",
+             nb::overload_cast<>(&PCG32::next_float32_n),
+             doc_PCG32_next_float32_n)
+        .def("next_float32_n",
+             nb::overload_cast<const Mask &>(&PCG32::next_float32))
+        .def("next_float64_n",
+             nb::overload_cast<>(&PCG32::next_float64_n),
+             doc_PCG32_next_float64_n)
+        .def("next_float64_n",
+             nb::overload_cast<const Mask &>(&PCG32::next_float64_n))
         .def("__add__", [](const PCG32 &a, const Int64 &x) -> PCG32 { return a + x; }, nb::is_operator(), doc_PCG32_add)
         .def("__iadd__", [](PCG32 *a, const Int64 &x) -> PCG32* { *a += x; return a; }, nb::is_operator(), doc_PCG32_iadd)
         .def("__sub__", [](const PCG32 &a, const Int64 &x) -> PCG32 { return a - x; }, nb::is_operator(), doc_PCG32_sub)
