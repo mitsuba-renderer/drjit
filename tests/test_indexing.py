1ebb23103877a2347d31616c94618db0f64f4aad
Wenzel Jakob <wenzel.jakob@epfl.ch>
2025-03-14 21:03:57 +0900

    Slice access/assignment for all Dr.Jit arrays
    Slice indexing into (non-tensor) Dr.Jit arrays currently fails with a
    somewhat infuriating error message:
    
    ```text
    Complex slicing operations are currently only supported on tensors.
    ```
    
    This commit adds support for regular arrays to bring them to the
    same level. In particular, the following are now supported:
    
    - slice access and assignment (potentially nested). For example, the
      following is now legal for `dr.cuda.Array3f`:
    
      ```python
      array[:-1, 1:100:2] = ..``
      ```
    
      These operations are compiled into scatters/gathers.
    
    - Indexing/indexed assignment using integer arrays, e.g.
    
      ```python
      index : UInt32 = ...
      values: Float = ...
      values[index] = ...
      ```
    
    - Fully replacing the contents of an array with another array.
    
      ```python
      x[:] = y
      ```

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -64,7 +64,7 @@ def test03_index_nested(t):
     with pytest.raises(TypeError) as e:
         v[-1, ...]
 
-    assert "Complex slicing operations are currently only supported on tensors." in str(e.value.__context__)
+    assert "Complex slicing operations involving 'None' / '...' are currently only supported on tensors" in str(e.value.__context__)
 
 @pytest.test_arrays('shape=(*), -bool', 'tensor, -bool')
 def test04_masked_assignment(t):
@@ -109,7 +109,7 @@ def test06_swizzle_assignment(t):
 
 
 @pytest.test_arrays('shape=(3, *), uint32')
-def test06_bad_swizzle(t):
+def test07_bad_swizzle(t):
     a = t(1,2,3)
     with pytest.raises(AttributeError):
         b = a.xw
@@ -121,3 +121,89 @@ def test06_bad_swizzle(t):
     with pytest.raises(IndexError):
         a = t(1,2,3)
         a.xywx = a.yyx
+
+
+@pytest.test_arrays('shape=(*), float32')
+def test08_1d_slice_get(t):
+    a = t(1,2,3,4,5)
+    assert dr.all(a[1::2] == t(2, 4))
+    assert dr.all(a[:-2] == t(1, 2, 3))
+    assert a[:].index == a.index
+
+
+@pytest.test_arrays('shape=(*), float32')
+def test09_1d_slice_set(t):
+    a = t(1,2,3,4,5)
+    a[1::2] = t(8, 9)
+    assert dr.all(a == t(1,8,3,9,5))
+
+
+@pytest.test_arrays('shape=(*), float32')
+def test10_1d_slice_get_uint32_index(t):
+    a = t(1,2,3,4,5)
+    u = dr.uint32_array_t(t)
+    i = dr.int32_array_t(t)
+    assert dr.all(a[u(1, 3)] == t(2, 4))
+    assert dr.all(a[i(-1, -2)] == t(5, 4))
+
+
+@pytest.test_arrays('shape=(*), float32')
+def test11_1d_slice_set_uint32_index(t):
+    a = t(1,2,3,4,5)
+    u = dr.uint32_array_t(t)
+    i = dr.int32_array_t(t)
+    a[u(1, 3)] = t(8, 9)
+    a[i(-1, -3)] = t(0, 1)
+    assert dr.all(a == t(1,8,1,9,0))
+
+@pytest.test_arrays('shape=(3, *), float32', 'shape=(*, *), float32')
+def test12_2d_slice_get(t):
+    v = dr.value_t(t)
+    a = t(1,2,3)
+    a1 = a[-1]
+    assert type(a1) is v and a1 == 3
+    a12 = a[1:3]
+    assert len(a12) == 2 and dr.all(a12 == [2,3])
+    a12 = a[:-1]
+    assert len(a12) == 2 and dr.all(a12 == [1,2])
+
+@pytest.test_arrays('shape=(3, *), float32', 'shape=(*, *), float32')
+def test13_2d_slice_set(t):
+    v = dr.value_t(t)
+    a = t(1,2,3)
+    a[1:3] = [10, 20]
+    a[:-1] = [100, 200]
+    assert dr.all(a == [100, 200, 20], axis=None)
+    a[1:3] = 1
+    assert dr.all(a == [100, 1, 1], axis=None)
+
+@pytest.test_arrays('shape=(*), float32')
+def test14_overwrite_1d(t):
+    x = t([1, 2])
+    x_id = x.index
+    x_backup = x
+    x[:] = t([2,3])
+    assert dr.all(x == t([2,3]))
+    if dr.is_jit_v(t):
+        assert x.index != x_id and x is x_backup
+    x[:] = 0
+    assert dr.all(x == t([0,0]))
+
+@pytest.test_arrays('tensor, float32')
+def test15_overwrite_tensor(t):
+    x = t([1, 2])
+    x_id = x.array.index
+    x_backup = x
+    x[:] = t([2,3])
+    assert dr.all(x == t([2,3]))
+    if dr.is_jit_v(t):
+        assert x.array.index != x_id and x is x_backup
+    x[:] = 0
+    assert dr.all(x == t([0,0]))
+
+
+@pytest.test_arrays('shape=(3, *), float32, jit')
+def test16_nested_slice_assignment(t):
+    x = dr.ones(t, (3, 20))
+    x[:-1, 1:100:2] = 2
+    assert dr.any(x == 2, axis=None)
