4c29b6780cc8a8104529289bcdf51fbc19e3570f
Lovro Nuic <12744626+lnuic@users.noreply.github.com>
2025-05-27 05:44:26 +0200

    pcg32: reverse number generation

--- tests/test_rand.py
+++ tests/test_rand.py
@@ -64,3 +64,73 @@ def test03_seed(t):
 
     assert not dr.all(v0 == v1) and dr.all(v0 == v2)
     assert not dr.all(v0 == v3) and not dr.all(v1 == v3) and dr.all(v3 == v4)
+
+@pytest.test_arrays('shape=(*), float32')
+def test04_prev_sample(t):
+    m = sys.modules[t.__module__]
+    pcg = m.PCG32()
+    
+    uint32 = pcg.next_uint32()
+    uint32_prev = pcg.prev_uint32()
+    assert uint32 == uint32_prev
+
+    uint64 = pcg.next_uint64()
+    uint64_prev = pcg.prev_uint64()
+    assert uint64 == uint64_prev
+
+    f32 = pcg.next_float32()
+    f32_prev = pcg.prev_float32()
+    assert f32 == f32_prev
+
+    f64 = pcg.next_float64()
+    f64_prev = pcg.prev_float64()
+    assert f64 == f64_prev
+
+    f16 = pcg.next_float16()
+    f16_prev = pcg.prev_float16()
+    assert f16 == f16_prev
+
+    f16_n = pcg.next_float16_normal()
+    f16_n_prev = pcg.prev_float16_normal()
+    assert f16_n == f16_n_prev
+
+    f32_n = pcg.next_float32_normal()
+    f32_n_prev = pcg.prev_float32_normal()
+    assert f32_n == f32_n_prev
+
+    f64_n = pcg.next_float64_normal()
+    f64_n_prev = pcg.prev_float64_normal()
+    assert f64_n == f64_n_prev
+
+    mask = m.Bool([True, False])
+    uint32_masked = pcg.next_uint32(mask)
+    uint32_masked_prev = pcg.prev_uint32(mask)
+    assert dr.all(uint32_masked == uint32_masked_prev)
+
+    uint64_masked = pcg.next_uint64(mask)
+    uint64_masked_prev = pcg.prev_uint64(mask)
+    assert dr.all(uint64_masked == uint64_masked_prev)
+
+    f16_masked = pcg.next_float16(mask)
+    f16_masked_prev = pcg.prev_float16(mask)
+    assert dr.all(f16_masked == f16_masked_prev)
+
+    f32_masked = pcg.next_float32(mask)
+    f32_masked_prev = pcg.prev_float32(mask)
+    assert dr.all(f32_masked == f32_masked_prev)
+
+    f64_masked = pcg.next_float64(mask)
+    f64_masked_prev = pcg.prev_float64(mask)
+    assert dr.all(f64_masked == f64_masked_prev)
+
+    f16_n_masked = pcg.next_float16_normal(mask)
+    f16_n_masked_prev = pcg.prev_float16_normal(mask)
+    assert dr.all(f16_n_masked == f16_n_masked_prev)
+
+    f32_n_masked = pcg.next_float32_normal(mask)
+    f32_n_masked_prev = pcg.prev_float32_normal(mask)
+    assert dr.all(f32_n_masked == f32_n_masked_prev)
+
+    f64_n_masked = pcg.next_float64_normal(mask)
+    f64_n_masked_prev = pcg.prev_float64_normal(mask)
+    assert dr.all(f64_n_masked == f64_n_masked_prev)
