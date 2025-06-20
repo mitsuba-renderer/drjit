81590099eb85bc36237515bfca27d49ad2fe15dc
Christian Döring <christian.doering@tum.de>
2025-06-20 11:23:07 +0200

    Small suggestions from Merlin

--- tests/test_hashgrid.py
+++ tests/test_hashgrid.py
@@ -13,12 +13,12 @@ def test01_hash_grid_tcnn(t):
     try:
         import tinycudann as tcnn
     except ImportError:
-        pytest.skip("This test requires PyTorch to be installed.")
+        pytest.skip("This test requires tinycudann to be installed.")
 
     try:
         import torch
     except ImportError:
-        pytest.skip("This test requires tinycudann to be installed.")
+        pytest.skip("This test requires PyTorch to be installed.")
 
     m = sys.modules[t.__module__]
     Float16 = m.Float16
@@ -60,10 +60,10 @@ def test01_hash_grid_tcnn(t):
     hg_ref = tcnn.Encoding(
         3,
         config,
-        dtype = torch.float32,
+        dtype=torch.float32,
     )
     data = hg_ref.params.data
-    data = torch.linspace(0, 1_000, data.shape[0], device = device, dtype = torch.float32)
+    data = torch.linspace(0, 1_000, data.shape[0], device=device, dtype=torch.float32)
     hg_ref.params.data = data.cuda()
 
     for param in hg_ref.parameters():
@@ -76,7 +76,7 @@ def test01_hash_grid_tcnn(t):
 
     x = [sampler.next_float32(), sampler.next_float32(), sampler.next_float32()]
     dr.make_opaque(x)
-    x_torch = torch.stack([xx.torch() for xx in x], dim=1).to(device = "cuda")
+    x_torch = torch.stack([xx.torch() for xx in x], dim=1).to(device="cuda")
 
     res = hg(x)
 
@@ -86,27 +86,21 @@ def test01_hash_grid_tcnn(t):
     start.record()
     ref = hg_ref(x_torch)
     end.record()
-
     torch.cuda.synchronize()
 
     ref = ref.to(device)
-
     res_torch = res.torch().permute(1, 0)
-
     assert torch.allclose(res_torch, ref)
 
     ## gradients
 
     res = ArrayXf(res)
-
     loss_res = dr.mean(dr.square(res - 1), axis=None)
 
     dr.backward(loss_res)
 
     loss_ref = torch.mean(torch.square(ref - 1), dim=None)
-
     loss_ref.backward()
-
     grad_res = dr.grad(hg.data).torch()
     grad_ref = hg_ref.params.grad.to(device=device)
 
@@ -132,11 +126,9 @@ def test02_permutohedral(t):
     Float32 = m.Float32
 
     encoding = encoding.alloc(Float16)
-
     encoding.set_params(dr.rand(Float16, (dr.width(encoding.data))))
 
     x = [dr.rand(Float32, n) for i in range(d)]
-
     res = encoding(x)
 
     # TODO: validate results
