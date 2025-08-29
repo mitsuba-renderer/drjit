fd69305652ddfccdd8c8fcdb36d07e43718e4c59
Lovro Nuic <12744626+lnuic@users.noreply.github.com>
2025-08-29 16:48:04 +0200

    test: add coverage for ad repeat/tile c++ operations

--- tests/test_py_cpp_consistency_ext.py
+++ tests/test_py_cpp_consistency_ext.py
@@ -28,3 +28,37 @@ def test02_repeat(t):
     pkg = get_pkg(t)
     x = dr.arange(t, 10)
     assert dr.all(pkg.repeat(x, 3) == dr.repeat(x, 3))
+
+@pytest.test_arrays('float32,is_diff,shape=(*)')
+def test03_tile_ad(t):
+    pkg = get_pkg(t)
+    x = dr.arange(t, 10)
+    x_tiled_dr = dr.tile(x, 3)
+    x_tiled_pkg = pkg.tile(x, 3)
+
+    dr.enable_grad(x_tiled_dr, x_tiled_pkg)
+    
+    x2_tiled_dr = x_tiled_dr * x_tiled_dr
+    x2_tiled_pkg = x_tiled_pkg * x_tiled_pkg
+
+    dr.backward(x2_tiled_dr)
+    dr.backward(x2_tiled_pkg)
+
+    assert dr.all(dr.grad(x_tiled_dr) == dr.grad(x_tiled_pkg))
+
+@pytest.test_arrays('float32,is_diff,shape=(*)')
+def test04_repeat_ad(t):
+    pkg = get_pkg(t)
+    x = dr.arange(t, 10)
+    x_repeated_dr = dr.repeat(x, 3)
+    x_repeated_pkg = pkg.repeat(x, 3)
+
+    dr.enable_grad(x_repeated_dr, x_repeated_pkg)   
+
+    x2_repeated_dr = x_repeated_dr * x_repeated_dr
+    x2_repeated_pkg = x_repeated_pkg * x_repeated_pkg
+
+    dr.backward(x2_repeated_dr)
+    dr.backward(x2_repeated_pkg)
+
+    assert dr.all(dr.grad(x_repeated_dr) == dr.grad(x_repeated_pkg))
