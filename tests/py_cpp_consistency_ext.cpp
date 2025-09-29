ee04e6c4708a17bd83b8cef3dd0c9abcf62f6a76
Nicolas Roussel <n.roussel13@gmail.com>
2025-09-29 14:01:01 +0200

    Add Python<->C++ consistency tests for `transform_compose` & `transform_decompose`

--- tests/py_cpp_consistency_ext.cpp
+++ tests/py_cpp_consistency_ext.cpp
@@ -2,6 +2,9 @@
 #include <drjit/python.h>
 #include <drjit/autodiff.h>
 #include <drjit/packet.h>
+#include <drjit/matrix.h>
+#include <drjit/transform.h>
+#include <nanobind/stl/tuple.h>
 
 namespace nb = nanobind;
 namespace dr = drjit;
@@ -16,11 +19,34 @@ Float repeat(const Float &source, uint32_t count) {
     return Float::steal(jit_var_repeat(source.index(), count));
 }
 
+template <typename Matrix4, typename Matrix3, typename Quaternion, typename Array>
+std::tuple<Matrix3, Quaternion, Array> transform_decompose(Matrix4 m) {
+    auto [s, q, t] = dr::transform_decompose(m);
+    return std::make_tuple(s, q, t);
+}
+
+template <typename Matrix4, typename Matrix3, typename Quaternion, typename Array>
+Matrix4 transform_compose(Matrix3 m, Quaternion q, Array tr) {
+    return dr::transform_compose<Matrix4>(m, q, tr);
+}
+
+template <typename Matrix4, typename Array>
+Matrix4 translate(Array tr) {
+    return dr::translate<Matrix4>(tr);
+}
+
 template <JitBackend Backend> void bind(nb::module_ &m) {
     using Float = dr::DiffArray<Backend, float>;
+    using Matrix4f = dr::Matrix<dr::DiffArray<Backend, float>, 4>;
+    using Matrix3f = dr::Matrix<dr::DiffArray<Backend, float>, 3>;
+    using Quaternion4f = dr::Quaternion<dr::DiffArray<Backend, float>>;
+    using Array3f = dr::Array<dr::DiffArray<Backend, float>, 3>;
 
     m.def("tile", &tile<Float>);
     m.def("repeat", &repeat<Float>);
+    m.def("transform_decompose", &transform_decompose<Matrix4f, Matrix3f, Quaternion4f, Array3f>);
+    m.def("transform_compose", &transform_compose<Matrix4f, Matrix3f, Quaternion4f, Array3f>);
+    m.def("translate", &translate<Matrix4f, Array3f>);
 }
 
 NB_MODULE(py_cpp_consistency_ext, m) {
