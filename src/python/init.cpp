mitsuba-renderer/drjit

commit e7e1339921aa0db5dda3f7623684818548004186

Initialization of ArrayX[f/i..] from tensors
Previously, nested array types with multiple dynamic dimensions (like
ArrayXf) could not be initialized from Dr.Jit tensors or NumPy arrays.

This commit slightly generalizes the array initialization logic so that
this case is supported as well.

src/python/init.cpp
@@ -185,6 +185,7 @@ int tp_init_array(PyObject *self, PyObject *args, PyObject *kwds) noexcept {
                 if (has_dim) {
                     // Import flattened array in C-style ordering
                     nb::object flattened;
+                    dr::vector<size_t> source_shape_vec;
 
                     if (s.is_complex)
                         do_flip_axes = true;
@@ -207,13 +208,14 @@ int tp_init_array(PyObject *self, PyObject *args, PyObject *kwds) noexcept {
                                       d, s.shape[d], source_shape);
                         }
                         flattened = nb::steal(as.tensor_array(arg));
+                        source_shape_vec = shape;
                     } else {
-                        flattened = import_ndarray(s, arg);
+                        flattened = import_ndarray(s, arg, &source_shape_vec);
                     }
 
                     nb::object unraveled = unravel(
                         nb::borrow<nb::type_object_t<dr::ArrayBase>>(self_tp),
-                        flattened, do_flip_axes ? 'F' : 'C');
+                        flattened, do_flip_axes ? 'F' : 'C', &source_shape_vec);
 
                     nb::inst_move(self, unraveled);
                     return 0;

src/python/memop.cpp
@@ -801,20 +801,21 @@ static nb::object unravel_recursive(nb::handle dtype,
     } else {
         const ArraySupplement &s = supp(dtype);
 
-        nb::object result = dtype();
+        nb::list result_list;
         for (Py_ssize_t i = 0; i < shape[depth]; ++i) {
-            result[i] =
+            result_list.append(
                 unravel_recursive(s.value, value, index_dtype, shape, strides,
-                                  offset, depth + 1, stop_depth);
+                                  offset, depth + 1, stop_depth));
             offset += strides[depth];
         }
 
-        return result;
+        return dtype(result_list);
     }
 }
 
 nb::object unravel(const nb::type_object_t<ArrayBase> &dtype,
-                   nb::handle_t<ArrayBase> array, char order) {
+                   nb::handle_t<ArrayBase> array, char order,
+                   const vector<size_t> *shape_hint) {
     const ArraySupplement &s = supp(dtype);
     if (s.is_tensor)
         throw nb::type_error(
@@ -852,10 +853,15 @@ nb::object unravel(const nb::type_object_t<ArrayBase> &dtype,
     int ndim = s.ndim;
     for (int i = 0; i < ndim; ++i) {
         if (s.shape[i] == DRJIT_DYNAMIC) {
-            if (i != s.ndim - 1)
+            if (shape_hint && (size_t) i < shape_hint->size()) {
+                // Use the provided shape hint for this dynamic dimension
+                shape[i] = (Py_ssize_t) (*shape_hint)[i];
+            } else if (i != s.ndim - 1) {
                 throw nb::type_error("drjit.unravel(): only the last dimension "
                                      "of 'dtype' may be dynamic!");
-            shape[i] = size / stride;
+            } else {
+                shape[i] = size / stride;
+            }
         } else {
             shape[i] = s.shape[i];
         }
@@ -1132,7 +1138,7 @@ static nb::object reshape(nb::type_object dtype, nb::handle value,
             }
 
             return unravel(nb::borrow<nb::type_object_t<ArrayBase>>(dtype),
-                           raveled, order);
+                           raveled, order, &new_shape);
         }
     } catch (nb::python_error &e) {
         nb::raise_from(e, PyExc_RuntimeError,

src/python/memop.h
@@ -32,7 +32,8 @@ extern nb::object ravel(nb::handle h, char order,
                         const VarType *vt_in = nullptr);
 
 extern nb::object unravel(const nb::type_object_t<ArrayBase> &dtype,
-                          nb::handle_t<ArrayBase> array, char order);
+                          nb::handle_t<ArrayBase> array, char order,
+                          const vector<size_t> *shape_hint = nullptr);
 
 extern nb::object scatter_inc(nb::handle_t<drjit::ArrayBase> target,
                               nb::object index, nb::object active);

tests/test_init.py
@@ -684,3 +684,22 @@ def test30_init_ad_from_non_ad_with_cast(t, reverse):
     casted = TensorXf(TensorXi([[0, 1], [2, 3]]))
     assert type(casted) == TensorXf
     assert dr.all(casted == [[0, 1], [2, 3]], axis=None)
+
+
+@pytest.test_arrays('float32, shape=(*, *), jit')
+def test31_init_arrayx_from_tensor_and_ndarray(t):
+    # Test initialization of ArrayXf from tensors
+    np = pytest.importorskip("numpy")
+    mod = sys.modules[t.__module__]
+    TensorXf = mod.TensorXf
+
+    x = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], dtype=np.float32)
+
+    def check(result):
+        assert len(result) == 2
+        assert len(result[0]) == 5
+        assert dr.all(result[0] == [1, 2, 3, 4, 5])
+        assert dr.all(result[1] == [6, 7, 8, 9, 10])
+
+    check(t(x))
+    check(t(TensorXf(x)))

commit df4cf483cb6919b9bdf427c9e33faeb4f68982a7

Fix regression in ArrayX initialization from tensors/ndarrays
Commit e7e13399 introduced two bugs when initializing arrays with flipped
axes (complex types or explicit flip_axes=True):

1. The shape hint passed to unravel() was in the wrong order - it needs
   to be reversed when do_flip_axes is true to match the target dtype's
   dimension ordering.

2. In import_ndarray(), the code adding the complex dimension to shape_out
   had a broken shift loop and incorrectly set shape_out[0] to 0 instead
   of 2 (the real/imaginary dimension size).

src/python/init.cpp
@@ -213,6 +213,10 @@ int tp_init_array(PyObject *self, PyObject *args, PyObject *kwds) noexcept {
                         flattened = import_ndarray(s, arg, &source_shape_vec);
                     }
 
+                    if (do_flip_axes)
+                        std::reverse(source_shape_vec.begin(),
+                                     source_shape_vec.end());
+
                     nb::object unraveled = unravel(
                         nb::borrow<nb::type_object_t<dr::ArrayBase>>(self_tp),
                         flattened, do_flip_axes ? 'F' : 'C', &source_shape_vec);
@@ -587,9 +591,9 @@ nb::object import_ndarray(ArrayMeta m, PyObject *arg, vector<size_t> *shape_out,
     if (m.is_complex) {
         if (shape_out) {
             shape_out->resize(shape_out->size() + 1);
-            for (size_t i = 1; i < ndim; ++i)
+            for (size_t i = shape_out->size() - 1; i > 0; --i)
                 shape_out->operator[](i) = shape_out->operator[](i - 1);
-            shape_out->operator[](0) = 0;
+            shape_out->operator[](0) = 2;
         }
         ndim += 1;
         size *= 2;